3794baa5520bc61288487324fa0e88250fc7949e
root <root@build-node.(none)>
2012-11-21 00:01:34 -0800

    Fix the wrong Ip address for network plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,15 +34,14 @@ include pip
         }
 
 
-  file { "/etc/collectd/collectd.conf":
-        notify  => Service["collectd"],
-	ensure  => present,
-        owner   => "root",
-        group   => "root",
-        mode    => 0600,
-        source  => "puppet:///modules/collectd/collectd.conf",
-        require => Package["collectd"],
-    }
+  file { '/etc/collectd/collectd.conf':
+      group   => 'root',
+      mode    => '0644',
+      owner   => 'root',
+      require => Package['collectd'],
+      notify  => Service['collectd'],
+      content => template('collectd/collectd.erb'),
+   }
 
 
   file { "/etc/collectd/collectd-plugins":
